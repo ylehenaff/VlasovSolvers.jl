b45b3b28a46fecc6cfe7ca88f81489ac3d56f926
YLeHenaff <yoann.lehenaff@univ-rennes1.fr>
2022-05-31 14:52:21 +0200

    multiD

--- src/WPM.jl
+++ src/WPM.jl
@@ -13,14 +13,14 @@ using LinearAlgebra
 Describe meta particules, represented by a Dirac distribution in (``x``, ``v``), with a weight ``β``
 """
 struct Particles{T}
-    x :: Array{T, 2}    # list of the positions
-    v :: Array{T, 2}    # list of the velocities
-    β :: Vector{T}      # list of the weights of the particules
-    nbpart :: Int       # number of particules
-    dim :: Int
+    x::Array{T,2}    # list of the positions
+    v::Array{T,2}    # list of the velocities
+    β::Vector{T}      # list of the weights of the particules
+    nbpart::Int       # number of particules
+    dim::Int
     type
 
-    function Particles{T}(x, v, β) where T<:Real
+    function Particles{T}(x, v, β) where {T<:Real}
         new(x, v, β, size(x)[2], size(x)[1], T)
     end
 end
@@ -30,28 +30,28 @@ end
     and holds some pre-allocated arrays used for the time integration only.
 """
 struct symplectic_rkn4{T<:Real}
-    a ::    Array{T, 2}
-    b̄ ::    Vector{T}
-    c ::    Vector{T}
-    b ::    Vector{T}
-    dt ::   T
-    fg ::   Array{Array{T, 2}, 1}
-    G ::    Array{T, 2}
+    a :: Array{T,2}
+    b̄ :: Vector{T}
+    c :: Vector{T}
+    b :: Vector{T}
+    dt :: T
+    fg :: Array{Array{T,2},1}
+    G :: Array{T,2}
     nb_steps :: Int
 
-    function symplectic_rkn4{T}(X, dt) where T<:Real
+    function symplectic_rkn4{T}(X, dt) where {T<:Real}
         # a, b̄, c, b correspond to the Butcher tableau of Runge-Kutta-Nystrom 3steps order4.
-        a = [0.0        0.0         0.0; 
-            (2-√3)/12   0.0         0.0; 
-            0.0         √(3)/6      0.0]
-        b̄ = [(5 - 3*√3)/24,     (3+√3)/12,  (1+√3)/24]
-        c = [(3+√3)/6,          (3-√3)/6,   (3+√3)/6]
-        b = [(3-2*√3)/12,       1/2,        (3+2*√3)/12]
+        a = [0.0 0.0 0.0
+            (2-√3)/12 0.0 0.0
+            0.0 √(3)/6 0.0]
+        b̄ = [(5 - 3 * √3) / 24, (3 + √3) / 12, (1 + √3) / 24]
+        c = [(3 + √3) / 6, (3 - √3) / 6, (3 + √3) / 6]
+        b = [(3 - 2 * √3) / 12, 1 / 2, (3 + 2 * √3) / 12]
 
         stages = 3
 
-        new(a .* dt^2, b̄ .* dt^2, c .* dt, b .* dt, dt, 
-            [similar(X) for _=1:stages],  # fg
+        new(a .* dt^2, b̄ .* dt^2, c .* dt, b .* dt, dt,
+            [similar(X) for _ = 1:stages],  # fg
             similar(X), # G
             stages # number of steps
         )
@@ -60,27 +60,27 @@ end
 
 
 struct rkn5{T<:Real}
-    a :: Array{T, 2}
+    a :: Array{T,2}
     b̄ :: Vector{T}
     c :: Vector{T}
     b :: Vector{T}
     dt :: T
-    fg :: Array{T, 2}
-    G ::  Array{T, 1}
+    fg :: Array{T,2}
+    G :: Array{T,1}
     nb_steps :: Int
 
-    function rkn5{T}(X, dt) where T<:Real
+    function rkn5{T}(X, dt) where {T<:Real}
         # a, b̄, c, b correspond to the Butcher tableau of Runge-Kutta-Nystrom 3steps order4.
-        a = [0.0        0.0         0.0     0.0; 
-            1/50        0.0         0.0     0.0; 
-            -1/27       7/27        0.0     0.0;
-            3/10        -2/35       9/35    0.0]
-        b̄ = [14/336,    100/336,    54/336, 0.0]
-        c = [0,         1/5,        2/3,    1.0]
-        b = [14/336,    125/336,    162/336, 35/336]
+        a = [0.0 0.0 0.0 0.0
+            1/50 0.0 0.0 0.0
+            -1/27 7/27 0.0 0.0
+            3/10 -2/35 9/35 0.0]
+        b̄ = [14 / 336, 100 / 336, 54 / 336, 0.0]
+        c = [0, 1 / 5, 2 / 3, 1.0]
+        b = [14 / 336, 125 / 336, 162 / 336, 35 / 336]
 
         stages = 4
-        new(a .* dt^2, b̄ .* dt^2, c .* dt, b .* dt, dt, 
+        new(a .* dt^2, b̄ .* dt^2, c .* dt, b .* dt, dt,
             zeros(T, length(X), stages),  # fg
             similar(X, T), # G
             stages
@@ -92,35 +92,36 @@ end
 """
 Holds pre-allocated arrays
 """
-struct ParticleMover{T<:Real, DIM}
-    Φ :: Array{T, 2}
-    ∂Φ :: Array{T, 2}
-    meshx :: OneDGrid{T}
+struct ParticleMover{T<:Real,DIM}
+    Φ :: Array{T,1}
+    ∂Φ :: Array{T,2}
+    torus :: Vector{T}
     torus_size :: T
-    kx :: T
+    kxs :: Vector{T}
     K :: Int
-    C :: Array{T, DIM}
-    S :: Array{T, DIM}
-    tmpsinkcosk :: Array{T, 2}
+    C :: Array{T,DIM}
+    S :: Array{T,DIM}
+    tmpsinkcosk :: Array{T,2}
+    tmpsinkcoskᵗ :: Array{T,2}
     Eelec²tot² :: Vector{T} # Holds Eelec² and Etot²
-    momentum :: Array{T, 1}
+    momentum :: Array{T,1}
     rkn :: symplectic_rkn4{T}
     dt :: T
     type
     dim
 
-    function ParticleMover{T, DIM}(particles::Particles, meshx, K, dt; kx=1) where {T<:Real, DIM}
-        Φ = Array{T, 2}(undef, 2, particles.nbpart)
+    function ParticleMover{T,DIM}(particles::Particles, torus, K, dt) where {T<:Real,DIM}
+        Φ = Array{T, 1}(undef, particles.nbpart)
         ∂Φ = similar(particles.x)
-        tmpsinkcosk = Array{T, 2}(undef, 2, particles.nbpart)
-        dim = size(particles.x)[1]
+        tmpsinkcosk = Array{T,2}(undef, 2, particles.nbpart)
+        tmpsinkcoskᵗ = Array{T,2}(undef, particles.nbpart, 2)
 
         new(Φ,
             ∂Φ,
-            meshx, meshx.stop, kx, K, 
-            Array{T, DIM}(undef, fill(2K+1, DIM)...), #C
-            Array{T, DIM}(undef, fill(2K+1, DIM)...), #S
-            tmpsinkcosk,
+            convert.(T, torus), convert(T, *(torus...)), convert.(T, 2π ./ torus), K,
+            Array{T,DIM}(undef, fill(2K + 1, DIM)...), #C
+            Array{T,DIM}(undef, fill(2K + 1, DIM)...), #S
+            tmpsinkcosk, tmpsinkcoskᵗ,
             zeros(T, 2), # Eelec²tot² (tuple)
             similar(particles.v[:, 1]), # momentum
             symplectic_rkn4{T}(particles.x, dt), # rkn
@@ -137,22 +138,21 @@ end
 function RKN_timestepper!(p, pmover; kernel=kernel_poisson!)
     @views begin
         for s = 1:pmover.rkn.nb_steps
-            @. pmover.rkn.G = p.x + p.v * pmover.rkn.c[s];
+            @. pmover.rkn.G = p.x + p.v * pmover.rkn.c[s]
 
             for ss = 1:pmover.rkn.nb_steps
-                @. pmover.rkn.G +=  pmover.rkn.a[s, ss] * pmover.rkn.fg[ss];
+                @. pmover.rkn.G += pmover.rkn.a[s, ss] * pmover.rkn.fg[ss]
             end
-    
-            kernel(pmover.rkn.fg[s], pmover.rkn.G, p, pmover);
-        end
-    
-        @. p.x += pmover.dt * p.v;
-        for s=eachindex(pmover.rkn.b̄)
-            @. p.x += pmover.rkn.b̄[s] * pmover.rkn.fg[s];
-            @. p.v += pmover.rkn.b[s] * pmover.rkn.fg[s];
+            kernel(pmover.rkn.fg[s], pmover.rkn.G, p, pmover)
         end
 
-        kernel(pmover.∂Φ, p.x, p, pmover);
+        @. p.x += pmover.dt * p.v
+        for s = eachindex(pmover.rkn.b̄)
+            @. p.x += pmover.rkn.b̄[s] * pmover.rkn.fg[s]
+            @. p.v += pmover.rkn.b[s] * pmover.rkn.fg[s]
+        end
+
+        kernel(pmover.∂Φ, p.x, p, pmover)
     end
 end
 
@@ -170,7 +170,7 @@ end
 
     Updates X, V in place, and returns coefficients C, S at current time. 
 """
-function strang_splitting!(particles, pmover, kernel)  
+function strang_splitting!(particles, pmover, kernel)
     pmover.Φ .= 0
     pmover.∂Φ .= 0
     @. particles.x += particles.v * pmover.dt / 2
@@ -193,9 +193,9 @@ function kernel_poisson!(dst, x, p, pmover)
     pmover.S .= 0
 
     @views for idxk = CartesianIndices(pmover.C)
-        k = idxk.I .- (pmover.K + 1)
-        ξk = k .* 2π ./ pmover.meshx.stop
-        normξk² = sum(ξk.^2)
+        k = idxk.I .- (pmover.K+1)
+        ξk = k .* pmover.kxs
+        normξk² = sum(ξk .^ 2)
         (normξk² == 0 || sum(abs.(k)) > pmover.K) && continue
 
         @inbounds for (idx, pos) = enumerate(eachcol(x))
@@ -204,23 +204,25 @@ function kernel_poisson!(dst, x, p, pmover)
             pmover.S[idxk] += skck[1] * p.β[idx]
             pmover.C[idxk] += skck[2] * p.β[idx]
         end
-        
-        @. pmover.Φ[1, :] -= (pmover.C[idxk] * pmover.tmpsinkcosk[2, :] + pmover.S[idxk] * pmover.tmpsinkcosk[1, :]) / normξk²
+
+        # transpose!(pmover.tmpsinkcoskᵗ, pmover.tmpsinkcosk)
+
+        @. pmover.Φ -= pmover.C[idxk] * pmover.tmpsinkcosk[2, :] + pmover.S[idxk] * pmover.tmpsinkcosk[1, :] / normξk²
         # The line below computes -∂Φ[f](`x`) and stores it to `dst`. 
         # Changing dynamics : 
         #   "+=": repulsive potential (plasmas dynamics)
         #   "-=": attractive potential (galaxies dynamics)
-        @. dst[1, :] += transpose(transpose(pmover.C[idxk] * pmover.tmpsinkcosk[1, :] - pmover.S[idxk] * pmover.tmpsinkcosk[2, :]) .* ξk) / normξk²
-        # /!\ Vérifier que les dimensions sont bonnes sur la ligne au-dessus en dimension > 1 /!\
+        @. dst += (pmover.C[idxk] * pmover.tmpsinkcosk[1, :]' - pmover.S[idxk] * pmover.tmpsinkcosk[2, :]') * ξk / normξk²
+        
     end
-    
+
     dst ./= pmover.torus_size
     pmover.Φ ./= pmover.torus_size
 end
 
 
 function kernel_gyrokinetic!(dst, x, p, pmover)
-    dst .= .-x
+    @. dst = -x
 end
 
 
@@ -232,13 +234,14 @@ end
 """
 function compute_electricalenergy²!(p, pmover)
     pmover.Eelec²tot²[1] = 0.0
-    for idxk = eachindex(pmover.C) 
-        ξk = 2π .* (idxk .- (pmover.K+1)) ./ pmover.meshx.stop
-        normξk² = sum(ξk.^2)
-        (normξk² == 0 || sum(abs.(idxk .- (pmover.K+1))) > pmover.K)&&continue
+    @views for idxk = CartesianIndices(pmover.C)
+        k = idxk.I .- (pmover.K+1)
+        ξk = k .* pmover.kxs
+        normξk² = sum(ξk .^ 2)
+        (normξk² == 0 || sum(abs.(k)) > pmover.K) && continue
         pmover.Eelec²tot²[1] += (pmover.C[idxk]^2 + pmover.S[idxk]^2) / normξk²
     end
-    pmover.Eelec²tot²[1] /= pmover.torus_size;
+    pmover.Eelec²tot²[1] /= pmover.torus_size
 end
 
 
@@ -265,10 +268,10 @@ end
     Impose periodic boundary conditions in space.
 """
 function periodic_boundary_conditions!(p, pmover)
-    @views for idx=1:p.nbpart
-        for d=1:length(p.x[:, 1])
-            (p.x[d, idx] > pmover.meshx.stop)&&(p.x[d, idx] -= pmover.meshx.stop)
-            (p.x[d, idx] < 0)&&(p.x[d, idx] += pmover.meshx.stop) 
+    @views for idx = 1:p.nbpart
+        for d = 1:length(p.x[:, 1])
+            (p.x[d, idx] > pmover.torus[d]) && (p.x[d, idx] -= pmover.torus[d])
+            (p.x[d, idx] < 0) && (p.x[d, idx] += pmover.torus[d])
         end
     end
 end
@@ -280,12 +283,12 @@ function WPM_step!(p, pmover; kernel=kernel_poisson!)
     RKN_timestepper!(p, pmover; kernel) # 2 allocs, 288b
     # strang_splitting!(p, pmover, kernel)
     # strang_splitting_implicit!(p, pmover, kernel)
-    
+
     periodic_boundary_conditions!(p, pmover)
-    
+
     compute_momentum!(p, pmover) # 1 alloc, 64b
     compute_electricalenergy²!(p, pmover)
     compute_totalenergy²!(p, pmover)
-    
+
     return nothing
 end
